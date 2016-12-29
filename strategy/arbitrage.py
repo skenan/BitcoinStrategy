7703d70e0cf504db8e7eb5ce7bb5c95b47d28926
Sun Sun <skenan1992@gmail.com>
2016-12-29 11:21:06 -0800

    Adjust the calculate the trend

--- strategy/arbitrage.py
+++ strategy/arbitrage.py
@@ -5,10 +5,12 @@ from api.okcoin.OKcoinService import OKcoinService
 from tools import Utils
 from tools.Log import Log
 import time
+import numpy as np
 
 ORDER_RATIO = 0.8
 MAX_WAITING_TIME = 3
-
+MAX_HISTROY_SIZE = 8
+MAX_PRICE_DIFF = 12
 log = Log()
 
 
@@ -67,38 +69,48 @@ def calculate_tradatable_amount(avaliable_amount, buy_amount, sell_amount):
     return Qty if Qty > 0.01 else 0
 
 
-def calculate_price_trend(last_price, current_price):
+def calculate_price_trend(data):
     # 需要根据最近1分钟的价格变化进行计算
     # False指价格下跌趋势, True指价格上升趋势
-
-    return current_price - last_price > 0
+    x = np.arange(0, MAX_HISTROY_SIZE)
+    y = np.array(data)
+    z = np.polyfit(x, y, 1)
+    return z[0] > 0
 
 
 def go():
     huobi_service = HuobiService("BTC")
     okcoin_service = OKcoinService("BTC")
-
-    last_okcoin_depth = okcoin_service.getDepth(3)
-    last_huobi_depth = huobi_service.getDepth(3)
+    history_prices = []
+    while len(history_prices) < MAX_HISTROY_SIZE:
+        okcoin_depth = okcoin_service.getDepth(3)
+        history_prices.append(okcoin_depth.sell_price)
 
     while True:
         log.debug("正在分析..")
+
         # 先获取账户信息，耗时长
         huobi_account = huobi_service.getAccountInfo()
         okcoin_account = okcoin_service.getAccountInfo()
 
         log.info("可用人民币 %.4f, 可用比特币%.4f" % (
-        huobi_account.cny_balance + okcoin_account.cny_balance, huobi_account.btc_balance + okcoin_account.btc_balance))
+            huobi_account.cny_balance + okcoin_account.cny_balance,
+            huobi_account.btc_balance + okcoin_account.btc_balance))
         huobi_depth = huobi_service.getDepth(3)
         okcoin_depth = okcoin_service.getDepth(3)
+        history_prices.append(okcoin_depth.sell_price)
+        history_prices = history_prices[-MAX_HISTROY_SIZE:]
 
-        if (huobi_depth.buy_price - okcoin_depth.sell_price) > 0:  # 获利信号：OKcoin买，huobi卖
+        sprend1 = huobi_depth.buy_price - okcoin_depth.sell_price
+        sprend2 = okcoin_depth.buy_price - huobi_depth.sell_price
+
+        if 0 < sprend1 <= MAX_PRICE_DIFF:  # 获利信号：OKcoin买，huobi卖
             Qty = min(huobi_account.btc_balance, okcoin_account.cny_balance / okcoin_depth.sell_price)
             Qty = calculate_tradatable_amount(Qty, huobi_depth.buy_amount, okcoin_depth.sell_amount)
             if Qty:
                 log.info(
                     "获利信号：OKcoin买入 %.2f, Huobi卖出 %.2f, 数量 %.3f" % (okcoin_depth.sell_price, huobi_depth.buy_price, Qty))
-                if not calculate_price_trend(last_huobi_depth.buy_price, huobi_depth.buy_price):
+                if not calculate_price_trend(history_prices):
                     # 先huobi卖， 再okcoin买
                     exec_amount = sell(huobi_service, Qty, huobi_depth.buy_price, True)
                     if exec_amount:
@@ -109,13 +121,13 @@ def go():
                     if exec_amount:
                         real_amount = sell(huobi_service, Qty, huobi_depth.buy_price, False)
 
-        elif (okcoin_depth.buy_price - huobi_depth.sell_price) >= 0:  # 获利信号：OKcoin卖，huobi买
+        elif 0 <= sprend2 <= MAX_PRICE_DIFF:  # 获利信号：OKcoin卖，huobi买
             Qty = min(okcoin_account.btc_balance, huobi_account.cny_balance / huobi_depth.sell_price)
             Qty = calculate_tradatable_amount(Qty, okcoin_depth.buy_amount, huobi_depth.sell_amount)
             if Qty:
                 log.info(
                     "获利信号：Huobi买入 %.2f, Okcoin卖出 %.2f, 数量 %.3f" % (huobi_depth.sell_price, okcoin_depth.buy_price, Qty))
-                if calculate_price_trend(last_okcoin_depth.buy_price, okcoin_depth.buy_price):
+                if calculate_price_trend(history_prices):
                     # 先huobi买，再okcoin卖
                     exec_amount = buy(huobi_service, Qty, huobi_depth.sell_price, True)
                     if exec_amount:
@@ -125,6 +137,6 @@ def go():
                     exec_amount = sell(okcoin_service, Qty, okcoin_depth.buy_price, True)
                     if exec_amount:
                         real_amount = buy(huobi_service, Qty, huobi_depth.sell_price, False)
-        last_huobi_depth = huobi_depth
-        last_okcoin_depth = okcoin_depth
+        # last_huobi_depth = huobi_depth
+        # last_okcoin_depth = okcoin_depth
         time.sleep(1)
